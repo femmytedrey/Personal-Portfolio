a748429e88f33c16e2af6b53d294cbfa17af9728
femmytedrey <femmytedrey@gmail.com>
2024-01-12 12:12:03 +0100

    working

--- src/components/ChimpForm.js
+++ src/components/ChimpForm.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import { Newsletter } from './Newsletter'
 
 export const ChimpForm = () => {
     const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
   return (
-    <div>ChimpForm</div>
+    <>
+        <MailchimpSubscribe
+            url = {postUrl}
+            render={({subscribe, status, message }) => (
+                <Newsletter
+                    status={status}
+                    message={message}
+                    onValidated={formData => subscribe(formData)}
+                />
+            )}
+        />
+    </>
   )
 }
