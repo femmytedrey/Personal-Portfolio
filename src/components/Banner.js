568de7012c3176cb4d16f3b34cfb9de77cbf1149
femmytedrey <femmytedrey@gmail.com>
2024-01-15 07:42:19 +0100

    working on email

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import headerImg from '../assets/img/header-img.svg';
@@ -8,9 +8,10 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ['Front-end Developer', 'Graphics Designer', 'Logo Designer', 'UI/UX Designer'];
     const [text, setText] = useState('');
     const delta = useRef(300 - Math.random() * 100);
+
+    const toRotate = useMemo(() => ['Front-end Developer', 'Graphics Designer', 'Logo Designer', 'UI/UX Designer'], []);
 
     const tick = useCallback(() => {
         let i = loopNum % toRotate.length;
@@ -53,13 +54,8 @@ export const Banner = () => {
                                 <div className={`animate__animated ${isVisible ? 'animate__fadeIn' : ''}`}>
                                     <span className='tagline'>Welcome to my Portfolio</span>
                                     <h1>{`Hi I'm FemiDev! `}<span className='wrap'>{text}</span></h1>
-                                    <p>
-                                        I'm a frontend developer with a passion for crafting seamless and engaging digital experiences. From intuitive user interfaces
-                                        to responsive designs, I bring ideas to life. Let's collaborate to enhance your online presence!
-                                    </p>
-                                    <a href='#connect' className='letsconnect'>
-                                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
-                                    </a>
+                                    <p>I'm a frontend developer with a passion for crafting seamless and engaging digital experiences. From intuitive user interfaces to responsive designs, I bring ideas to life. Let's collaborate to enhance your online presence!</p>
+                                    <a href='#connect' className='letsconnect'><button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button></a>
                                 </div>
                             )}
                         </TrackVisibility>
