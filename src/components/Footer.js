743cbb5fb90b79e8cc2bb0abca7a4cf8b47a4609
femmytedrey <femmytedrey@gmail.com>
2024-01-15 16:08:12 +0100

    styling for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { ChimpForm } from './ChimpForm'
 import logo from '../assets/img/logo.png'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import { Github } from 'react-bootstrap-icons';
 
 export const Footer = () => {
   return (
@@ -16,10 +16,10 @@ export const Footer = () => {
                     <img src={logo} alt="logo" className='footer-logo'/>
                 </Col>
                 <Col sm={6} className='text-center text-sm-end'>
-                    <div className='social-icon'>
+                    <div className='social-icon mt-3'>
                         <a href='https://www.linkedin.com/in/ayomide-adeyemo-61865526b/' target='_blank' rel="noopener noreferrer"><img src={navIcon1} alt='Linkedin' /></a>
                         <a href='https://web.facebook.com/adeyemo.ayomide.165/' target='_blank' rel="noopener noreferrer"><img src={navIcon2} alt='facebook' /></a>
-                        <a href='https://www.instagram.com/only1femmytedrey?igshid=OGQ5ZDc2OK2ZA==' target='_blank' rel="noopener noreferrer"><img src={navIcon3} alt='instagram' /></a>
+                        <a href='https://github.com/femmytedrey/' target='_blank' rel="noopener noreferrer"><Github className='github' /></a>
                     </div>
                     <p>CopyRIght 2024. All Right Reserved</p>
                 </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import { Github } from 'react-bootstrap-icons';
 
 export const NavBar = () => {
     const [ activeLink, setActiveLink ] = useState('home');
@@ -43,10 +43,10 @@ export const NavBar = () => {
               <Nav.Link href="#projects" className={`mobile-nav-menu ${activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}`} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
         </Nav>
           <span className='navbar-text'>
-            <div className='social-icon'>
+            <div className='social-icon m-social-icon'>
                 <a href='https://www.linkedin.com/in/ayomide-adeyemo-61865526b/' target='_blank' rel="noopener noreferrer"><img src={navIcon1} alt='' /></a>
                 <a href='https://web.facebook.com/adeyemo.ayomide.165/' target='_blank' rel="noopener noreferrer"><img src={navIcon2} alt='' /></a>
-                <a href='https://www.instagram.com/only1femmytedrey?igshid=OGQ5ZDc2OK2ZA==' target='_blank' rel="noopener noreferrer"><img src={navIcon3} alt='' /></a>
+                <a href='https://github.com/femmytedrey/' target='_blank' rel="noopener noreferrer"><Github className='github' /></a>
             </div>
             <Nav.Link href='#connect'><button className='vvo' onClick={() => console.log('connect')}><span>Let's Connect</span></button></Nav.Link>
           </span>
