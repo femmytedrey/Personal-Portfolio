996066873de8aefbd8e7b75807e99325581c1921
femmytedrey <femmytedrey@gmail.com>
2024-07-07 15:30:51 +0100

    updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'
-// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDj0Xqe-zFGyrEUpsNzXv0CUxgHCto9XiM",
-  authDomain: "my-portfolio-65091.firebaseapp.com",
-  projectId: "my-portfolio-65091",
-  storageBucket: "my-portfolio-65091.appspot.com",
-  messagingSenderId: "386604185650",
-  appId: "1:386604185650:web:beeaea3e9edcf1c9b4642b",
-  measurementId: "G-3J60PSWGMQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
-export const storage = getStorage(app)
-// const analytics = getAnalytics(app);
+
+export const storage = getStorage(app);
