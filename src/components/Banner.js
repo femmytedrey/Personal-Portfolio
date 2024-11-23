1ea072f547e02015888fcafbb85da066a8cd2f05
femmytedrey <femmytedrey@gmail.com>
2024-11-23 20:08:34 +0100

    updating portfolio and code refactoring

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,70 +1,104 @@
-import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from '../assets/img/header-img.svg';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+} from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const delta = useRef(300 - Math.random() * 100);
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const delta = useRef(300 - Math.random() * 100);
 
-    const toRotate = useMemo(() => ['Front-end Developer', 'Graphics Designer', 'Logo Designer', 'UI/UX Designer'], []);
+  const toRotate = useMemo(
+    () => [
+      "Frontend Developer",
+      "Backend Developer",
+      "Graphics Designer",
+      "Logo Designer",
+      "UI/UX Designer",
+    ],
+    []
+  );
 
-    const tick = useCallback(() => {
-        let i = loopNum % toRotate.length;
-        let fulltext = toRotate[i];
-        let updatedText = isDeleting ? fulltext.substring(0, text.length - 1) : fulltext.substring(0, text.length + 1);
+  const tick = useCallback(() => {
+    let i = loopNum % toRotate.length;
+    let fulltext = toRotate[i];
+    let updatedText = isDeleting
+      ? fulltext.substring(0, text.length - 1)
+      : fulltext.substring(0, text.length + 1);
 
-        setText(updatedText);
+    setText(updatedText);
 
-        if (isDeleting) {
-            delta.current /= 2;
-        }
+    if (isDeleting) {
+      delta.current /= 2;
+    }
 
-        if (!isDeleting && updatedText === fulltext) {
-            setIsDeleting(true);
-            delta.current = 2000; // Set to your desired period
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum((prevLoopNum) => prevLoopNum + 1);
-            delta.current = 500;
-        }
-    }, [loopNum, text, isDeleting, toRotate]);
+    if (!isDeleting && updatedText === fulltext) {
+      setIsDeleting(true);
+      delta.current = 2000; // Set to your desired period
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum((prevLoopNum) => prevLoopNum + 1);
+      delta.current = 500;
+    }
+  }, [loopNum, text, isDeleting, toRotate]);
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta.current);
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta.current);
 
-        return () => {
-            clearInterval(ticker);
-        };
-    }, [tick]);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [tick]);
 
-    return (
-        <section className='banner' id='home'>
-            <Container>
-                <Row className='align-items-center'>
-                    <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                            {({ isVisible }) => (
-                                <div className={`animate__animated ${isVisible ? 'animate__fadeIn' : ''}`}>
-                                    <span className='tagline'>Welcome to my Portfolio</span>
-                                    <h1>{`Hi I'm FemiDev! `}<span className='wrap'>{text}</span></h1>
-                                    <p>I'm a frontend developer with a passion for crafting seamless and engaging digital experiences. From intuitive user interfaces to responsive designs, I bring ideas to life. Let's collaborate to enhance your online presence!</p>
-                                    <a href='#connect' className='letsconnect'><button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button></a>
-                                </div>
-                            )}
-                        </TrackVisibility>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt='Header' />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={`animate__animated ${
+                    isVisible ? "animate__fadeIn" : ""
+                  }`}
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi I'm FemiDev! `}
+                    <span className="wrap">{text}</span>
+                  </h1>
+                  <p>
+                    I'm a web developer with expertise in both frontend and
+                    backend development. From crafting intuitive user interfaces
+                    to building robust backend systems, I create seamless and
+                    scalable digital experiences. Let's collaborate to bring
+                    your ideas to life and elevate your online presence!{" "}
+                  </p>
+                  <a href="#connect" className="letsconnect">
+                    <button onClick={() => console.log("connect")}>
+                      Let's connect <ArrowRightCircle size={25} />
+                    </button>
+                  </a>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,106 +1,104 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from "react-bootstrap";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg'
-import meter2 from '../assets/img/meter2.svg'
-import meter3 from '../assets/img/meter3.svg'
-import colorSharp from '../assets/img/color-sharp.png'
-import { getDownloadURL, ref } from 'firebase/storage';
-import { storage } from '../config/firebase';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../config/firebase";
 
 export const Skills = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-    };
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-    const [cvDownloadURL, setCvDownloadURL] = useState('');
+  const skills = [
+    { id: 1, name: "HTML/CSS", icon: meter1 },
+    { id: 2, name: "Bootstrap", icon: meter2 },
+    { id: 3, name: "Tailwind CSS", icon: meter1 },
+    { id: 4, name: "JavaScript", icon: meter3 },
+    { id: 5, name: "React JS | Next JS", icon: meter1 },
+    { id: 6, name: "Vue JS", icon: meter3 },
+    { id: 7, name: "Node JS | Express JS", icon: meter3 },
+    { id: 8, name: "UI/UX & Graphics Design", icon: meter1 },
+    { id: 9, name: "Logo Design", icon: meter1 },
+  ];
 
-    const handleDownloadCV = () => {
-        window.open(cvDownloadURL, '_blank');
-    };
+  const [cvDownloadURL, setCvDownloadURL] = useState("");
 
-    useEffect(() => {
-        const cvStorageRef = ref(storage, 'mycv/My Resume.pdf');
-    
-        getDownloadURL(cvStorageRef)
-            .then((url) => {
-                setCvDownloadURL(url);
-            })
-            .catch((error) => {
-                console.error('Error getting download URL:', error);
-    
-                // If needed, you can set a default or show an error message to the user
-                // setCvDownloadURL(DEFAULT_CV_URL);
-            });
-    }, []);
-    
+  const handleDownloadCV = () => {
+    window.open(cvDownloadURL, "_blank");
+  };
 
-    
+  useEffect(() => {
+    const cvStorageRef = ref(storage, "mycv/My Resume.pdf");
 
+    getDownloadURL(cvStorageRef)
+      .then((url) => {
+        setCvDownloadURL(url);
+      })
+      .catch((error) => {
+        console.error("Error getting download URL:", error);
+      });
+  }, []);
 
   return (
     <section className="skill" id="skills">
-        <Container>
-            <Row>
-                <Col>
-                    <div className="skill-bx">
-                        <h2>
-                            Skills
-                        </h2>
-                        <p>In the realm of web development, I bring a versatile set of skills and expertise.<br></br> 
-                            My proficiency includes:
-                        </p>
-                        <Carousel responsive={responsive} infinite = {true}  className="skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt = "meter1" />
-                                <h5>HTML/CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt = "meter2" />
-                                <h5>BOOTSTRAP</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt = "meter1" />
-                                <h5>TAILWIND CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt = "meter3" />
-                                <h5>JAVASCRIPT / REACT JS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt = "meter4" />
-                                <h5>UIUX & GRAPHICS DESIGN</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt = "meter1" />
-                                <h5>LOGO DESIGNER</h5>
-                            </div>
-                        </Carousel>
-                        <button className="btn col-12 col-sm-12 col-md-6 col-lg-5 py-2 mt-5 downloadbtn" onClick={handleDownloadCV}>Download CV</button>
-                    </div>
-                    
-                </Col>
-                
-            </Row>
-        </Container>
-        <img className="background-image-left" src={colorSharp} alt="colorsharp" />
-        </section>
-  )
-}
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                In the realm of web development, I bring a versatile set of
+                skills and expertise.
+                <br />
+                My proficiency includes:
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                {skills.map((skill) => (
+                  <div className="item" key={skill.id}>
+                    <img src={skill.icon} alt={skill.name} />
+                    <h5>{skill.name}</h5>
+                  </div>
+                ))}
+              </Carousel>
+              <button
+                className="btn col-12 col-sm-12 col-md-6 col-lg-5 py-2 mt-5 downloadbtn"
+                onClick={handleDownloadCV}
+              >
+                Download CV
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img
+        className="background-image-left"
+        src={colorSharp}
+        alt="colorsharp"
+      />
+    </section>
+  );
+};
